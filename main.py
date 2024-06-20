c0776ca91fa1bdc3320f04ebaca10197b723bed1
sorapu <166776903+sorapu@users.noreply.github.com>
2024-06-20 18:26:52 +0900

    naosi

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "takoyaki"}
+    return {"message": "hello"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
